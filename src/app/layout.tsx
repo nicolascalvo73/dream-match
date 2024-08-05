bcf1df653521f48cb33ec369f5cb56fd1327978d
nicolascalvo73 <nicolascalvo73@gmail.com>
2024-08-05 13:09:28 -0300

    acomodando readme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
 	title: 'Challenge ATC - Nicolas Calvo',
 	description: 'Create your Dream Match',
+	icons: {
+		icon: '/favicon.ico',
+	},
 }
 
 export default function RootLayout({
